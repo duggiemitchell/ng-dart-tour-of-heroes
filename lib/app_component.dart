c4ef3b85d33b22af4f693702d8c0dcb615c97252
Erica Mitchell <mitchel.erica31@gmail.com>
2018-02-23 11:43:52 -0500

    saving wip

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,28 @@
 import 'package:angular/angular.dart';
+import 'dart:async';
+import 'src/hero.dart';
+import 'src/hero_detail_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  directives: const [CORE_DIRECTIVES, HeroDetailComponent],
+  templateUrl: 'app_component.html',
+  styleUrls: const ['app_component.css'],
+  providers: const [HeroService]
 )
-class AppComponent {
-  var name = 'Angular';
+class AppComponent implements OnInit {
+  final title = 'Tour of Heroes';
+  final HeroService _heroService; //  private property on AppComponent
+
+  List<Hero> heroes;
+  Hero selectedHero;
+  AppComponent(this._heroService); //  constructor to initialize service
+
+  Future<Null> getHeroes() async => heroes = await _heroService.getHeroes();
+  void ngOnInit() => getHeroes();
+  void onSelect(Hero hero) => selectedHero = hero;
+
+
+
 }
